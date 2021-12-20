f166b768c2cc368a034fc1188a4fdd6135f0fe63
kalinoy <kalinoy1@gmail.com>
2021-12-20 13:28:25 +0200

    Creating project

---
+++ chickenfarms-app/src/main/java/com/chickenfarms/chickenfarms/ChickenfarmsApplication.java
@@ -0,0 +1,13 @@
+package com.chickenfarms.chickenfarms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChickenfarmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChickenfarmsApplication.class, args);
+	}
+
+}
---
+++ chickenfarms-app/src/test/java/com/chickenfarms/chickenfarms/ChickenfarmsApplicationTests.java
@@ -0,0 +1,12 @@
+package com.chickenfarms.chickenfarms;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChickenfarmsApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
